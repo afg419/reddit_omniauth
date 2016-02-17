fd983e05cbf5ead455426ce502ed8cead1136327
Aaron Greenspan <afg419@gmail.com>
2016-02-16 21:24:26 -0700

    Tests home page for non logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
+  before_action :subreddit_all
+
+  def subreddit_all
+    @sub = "all"
+  end
+
   def show
     if current_user
       @all = JSON.parse(reddit_service.get(url: "https://oauth.reddit.com/",
-                               path: 'r/all/top',
+                               path: "r/#{subreddit_all}/top",
                               token: current_user.token))
     else
       @all = JSON.parse(reddit_service.unauth_get_json)
--- spec/features/non_logged_in_all_pages_spec.rb
+++ spec/features/non_logged_in_all_pages_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.feature "NonLoggedInAllPages", type: :feature do
   scenario "root page displays default all page" do
     visit root_path
-    expect(page).to have_content("All")
-    expect(page).to have_content("Hot")
-    expect(page).to have_content("New")
-    expect(page).to have_content("Controversial")
-    expect(page).to have_content("Top")
+    within('.nav') do
+      expect(page).to have_content("All")
+      expect(page).to have_content("Hot")
+      expect(page).to have_content("New")
+      expect(page).to have_content("Controversial")
+      expect(page).to have_content("Top")
+    end
   end
 end
